694a77c8c43b61740883a5482c9bdbe1fb12ce2c
ryosei0730 <ryosei0730@outlook.jp>
2024-07-19 16:46:46 +0900

    レッスン２　画面確認　エラー

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from app import views
 
 urlpatterns = [
     path('', views.StoreView.as_view(), name='store'),
-    path('store/<int:pk>', views.StaffView.as_view(), name='staff')
-]
+    path('store/<int:pk>', views.StaffView.as_view(), name='staff'),
+    path('calender/<int:pk>', views.CalendarView.as_view(), name='calender'),
+    path('calender/<int:pk>/<int:year>/<int:month>/<int:day>', views.CalendarView.as_view(), name='calendar')
+    ]
