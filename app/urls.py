a1c242d6e93b894561839ab61a01baa5801112f1
ryosei0730 <ryosei0730@outlook.jp>
2024-07-21 22:38:56 +0900

    予約サイトチュートリアル

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,11 @@ from app import views
 urlpatterns = [
     path('', views.StoreView.as_view(), name='store'),
     path('store/<int:pk>', views.StaffView.as_view(), name='staff'),
-    path('calender/<int:pk>', views.CalendarView.as_view(), name='calender'),
-    path('calender/<int:pk>/<int:year>/<int:month>/<int:day>', views.CalendarView.as_view(), name='calendar')
+    path('calendar/<int:pk>', views.CalendarView.as_view(), name='calendar'),
+    path('calendar/<int:pk>/<int:year>/<int:month>/<int:day>/', views.CalendarView.as_view(), name='calendar'),
+    path('calendar/<int:pk>/<int:year>/<int:month>/<int:day>/<int:hour>', views.BookingView.as_view(), name='booking'),
+    path('thanks', views.ThanksView.as_view(), name='thanks'),
+    path('mypage/<int:year>/<int:month>/<int:day>/', views.MyPageView.as_view(), name='mypage'),
+    path('mypage/holiday/<int:year>/<int:month>/<int:day>/<int:hour>/', views.Holiday, name='holiday'),
+    path('mypage/delete/<int:year>/<int:month>/<int:day>/<int:hour>/', views.Delete, name='delete'),
     ]
